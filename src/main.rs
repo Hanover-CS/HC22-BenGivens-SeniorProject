27d516825bb548687748ce290ebffcc5f8ed3473
Benjamin Givens <btg99email@gmail.com>
2021-09-20 23:10:59 -0400

    Setup hello world in rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get, launch, routes};
+
+#[get("/")]
+fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello_world])
 }
