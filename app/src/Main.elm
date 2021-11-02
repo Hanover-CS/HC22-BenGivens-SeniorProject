88230d4d02b4ea6e335f78a241946a0a7c1cfe44
Benjamin Givens <btg99email@gmail.com>
2021-11-02 09:27:46 -0400

    Graph error frequency

--- app/src/Main.elm
+++ app/src/Main.elm
@@ -1,7 +1,10 @@
 module Main exposing (..)
 
 import Browser exposing (Document)
-import Html exposing (text)
+import Chart as C
+import Chart.Attributes as CA
+import Html as H
+import Html.Attributes as HA
 
 type alias Model = ()
 type alias Msg = ()
@@ -18,9 +21,38 @@ main =
 init : () -> ( Model, Cmd Msg )
 init _ = ( (), Cmd.none )
 
+
+type alias Data = { x : String, y : Float }
+
+data : List Data
+data =
+    [ { x = "E0277", y = 16 }
+    , { x = "E0308", y = 10 }
+    , { x = "E0599", y = 39 }
+    , { x = "E0609", y = 3 }
+    ]
+
 view model =
     { title = "Error Explorer"
-    , body = [ text "Hello from Elm!" ]
+    , body =
+        [ H.text "Error Code Summary"
+        , H.div
+            [ HA.style "height" "300px"
+            , HA.style "width" "300px"
+            , HA.style "position" "absolute"
+            , HA.style "left" "50px"
+            ]
+            [ C.chart
+            [ CA.height 300
+            , CA.width 300
+            ]
+            [ C.binLabels .x [ CA.moveDown 20 ]
+            , C.yLabels [ CA.withGrid ]
+            , C.bars []
+                [ C.bar .y [] ]
+                data
+            ]]
+        ]
     }
 
 update : Msg -> Model -> ( Model, Cmd Msg )
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use rocket::fs::{relative, NamedFile};
 use rocket::{get, launch, routes};
+use rocket::response::content::Json;
+use rocket_sync_db_pools::{rusqlite, database};
+use serde::Serialize;
+
+#[database("messages")]
+struct DatabaseConnection(rusqlite::Connection);
+
+#[derive(Serialize)]
+struct Message {
+    code: String,
+    message: String,
+}
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![app_html, app_js])
+    rocket::build()
+        .attach(DatabaseConnection::fairing())
+        .mount("/api/", routes!(messages))
+        .mount("/", routes![app_html, app_js])
 }
 
 #[get("/")]
@@ -14,4 +29,21 @@ async fn app_html() -> Option<NamedFile> {
 #[get("/app.js")]
 async fn app_js() -> Option<NamedFile> {
     NamedFile::open(relative!("app/app.js")).await.ok()
+}
+
+#[get("/messages")]
+async fn messages(conn: DatabaseConnection) -> Option<Json<String>> {
+    let messages: Vec<Message> = conn.run(|conn| { 
+        let mut stmt = conn.prepare("SELECT code, message FROM error")?;
+        let messages = stmt.query_map([], |row| Ok(
+            Message {
+            code: row.get(0)?,
+            message: row.get(1)?,
+        }))?;
+        let messages: Result<Vec<Message>, rusqlite::Error> = messages.collect(); 
+        messages
+    }
+    ).await.ok()?;
+
+    Some(Json(serde_json::to_string_pretty(&messages).ok()?)) 
 }
