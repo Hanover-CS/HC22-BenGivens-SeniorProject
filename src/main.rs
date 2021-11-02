88fe8686350eba0f3513059a681b4dbe697af3d5
Benjamin Givens <btg99email@gmail.com>
2021-11-02 10:25:08 -0400

    Support search api with Tantivy

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ use rocket::{get, launch, routes};
 use rocket::response::content::Json;
 use rocket_sync_db_pools::{rusqlite, database};
 use serde::Serialize;
+use tantivy::collector::TopDocs;
+use tantivy::query::QueryParser;
+use tantivy::{Index, ReloadPolicy};
+use tantivy::schema::{STORED, Schema, TEXT};
+use tantivy::Document;
+use tempfile::TempDir;
 
 #[database("messages")]
 struct DatabaseConnection(rusqlite::Connection);
@@ -17,7 +23,7 @@ struct Message {
 fn rocket() -> _ {
     rocket::build()
         .attach(DatabaseConnection::fairing())
-        .mount("/api/", routes!(messages))
+        .mount("/api/", routes!(messages, search))
         .mount("/", routes![app_html, app_js])
 }
 
@@ -46,4 +52,51 @@ async fn messages(conn: DatabaseConnection) -> Option<Json<String>> {
     ).await.ok()?;
 
     Some(Json(serde_json::to_string_pretty(&messages).ok()?)) 
+}
+
+#[get("/search/<query>")]
+async fn search(query: String, conn: DatabaseConnection) -> Option<Json<String>> {
+    let index_path = TempDir::new().ok()?;
+    let mut schema_builder = Schema::builder();
+    schema_builder.add_text_field("message", TEXT | STORED);
+    let schema = schema_builder.build();
+    
+    let index = Index::create_in_dir(&index_path, schema.clone()).ok()?;
+    let mut index_writer = index.writer(50_000_000).ok()?;
+    
+    let message_field = schema.get_field("message")?;
+    
+    let messages: Vec<Message> = conn.run(|conn| { 
+        let mut stmt = conn.prepare("SELECT code, message FROM error")?;
+        let messages = stmt.query_map([], |row| Ok(
+            Message {
+            code: row.get(0)?,
+            message: row.get(1)?,
+        }))?;
+        let messages: Result<Vec<Message>, rusqlite::Error> = messages.collect(); 
+        messages
+    }
+    ).await.ok()?;
+    
+    for message in messages {
+        let mut doc = Document::default();
+        doc.add_text(message_field, message.message);
+        index_writer.add_document(doc);
+    }
+    index_writer.commit().ok()?;
+    
+    let reader = index.reader_builder().reload_policy(ReloadPolicy::OnCommit).try_into().ok()?;
+    let searcher = reader.searcher();
+    let query_parser = QueryParser::for_index(&index, vec![message_field]);
+    let query = query_parser.parse_query(&query).ok()?;
+    
+    let results = searcher.search(&query, &TopDocs::with_limit(10)).ok()?;
+    
+    let mut messages = Vec::new();
+    for (_score, address) in results {
+        let retrieved_doc = searcher.doc(address).ok()?;
+        messages.push(schema.to_json(&retrieved_doc));
+    }
+    
+    Some(Json(serde_json::to_string_pretty(&messages).ok()?))
 }
