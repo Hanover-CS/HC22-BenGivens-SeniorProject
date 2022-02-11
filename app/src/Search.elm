3d49f4453030e840e9c20766bafcb3f98b767bfa
Benjamin Givens <btg99email@gmail.com>
2022-02-11 02:08:04 -0500

    Add sorting modes for search

--- app/src/Search.elm
+++ app/src/Search.elm
@@ -16,41 +16,68 @@ type SearchResult
     | Value (List ErrorMessage)
     | SearchError
 
+type SortMode
+    = Relevance
+    | Length
+
 type alias Model =
     { query : String
-    , search_result : SearchResult
+    , sortMode : SortMode
+    , searchResult : SearchResult
     }
 
 type Msg
     = UpdateQuery String
+    | UpdateSortMode (Maybe SortMode)
     | GotSearchResult (Result Http.Error (List ErrorMessage))
-
 
 view : Model -> Html Msg
 view model =
     H.div
         []
-        [ view_search_box
-        , view_search_result model.search_result
+        [ viewSearchBox
+        , viewSortModeBox
+        , viewSearchResult model.searchResult model.sortMode
         ]
     
-view_search_box : Html Msg
-view_search_box =
+viewSearchBox : Html Msg
+viewSearchBox =
     H.input
         [ HA.type_ "text"
         , HE.onInput UpdateQuery
         ]
         []
 
-view_search_result : SearchResult -> Html Msg
-view_search_result result =
+viewSortModeBox : Html Msg
+viewSortModeBox =
+    H.select
+        [ HE.onInput (parseSortMode >> UpdateSortMode) ]
+        [ H.option [ HA.value "Relevance" ] [ H.text "Relevance" ]
+        , H.option [ HA.value "Length" ] [ H.text "Length" ]
+        ]
+
+parseSortMode : String -> Maybe SortMode
+parseSortMode mode =
+    case mode of
+        "Relevance" -> Just Relevance
+        "Length" -> Just Length
+        _ -> Nothing
+
+viewSearchResult : SearchResult -> SortMode -> Html Msg
+viewSearchResult result sortMode =
     case result of
         Loading -> H.text "Loading..."
-        Value messages -> H.table [] (List.map view_error_message messages)
+        Value messages -> H.table [] (messages |> sort sortMode |> List.map viewErrorMessage )
         SearchError -> H.text "Error loading results..."
         
-view_error_message : ErrorMessage -> Html Msg
-view_error_message { message, code } =
+sort : SortMode -> List ErrorMessage -> List ErrorMessage
+sort sortMode messages =
+    case sortMode of
+        Relevance -> messages
+        Length -> List.sortBy (.message >> String.words >> String.concat >> String.length) messages
+    
+viewErrorMessage : ErrorMessage -> Html Msg
+viewErrorMessage { message, code } =
     H.tr
         [ HA.class "error-message-row"  ]
         [ "error[E" ++ code ++ "]: " ++ message |> H.text ]
@@ -58,9 +85,11 @@ view_error_message { message, code } =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        UpdateQuery query -> ( { model | query = query, search_result = Loading }, getSearchResult query )
-        GotSearchResult (Ok result) -> ( { model | search_result = Value result }, Cmd.none )
-        GotSearchResult (Err _) -> ( { model | search_result = SearchError }, Cmd.none )
+        UpdateQuery query -> ( { model | query = query, searchResult = Loading }, getSearchResult query )
+        UpdateSortMode (Just mode) -> ( { model | sortMode = mode }, Cmd.none )
+        UpdateSortMode Nothing -> ( { model | sortMode = Relevance }, Cmd.none )
+        GotSearchResult (Ok result) -> ( { model | searchResult = Value result }, Cmd.none )
+        GotSearchResult (Err _) -> ( { model | searchResult = SearchError }, Cmd.none )
 
 getSearchResult : String -> Cmd Msg
 getSearchResult query =
@@ -76,4 +105,4 @@ errorMessageDecoder =
         ( Decode.field "message" Decode.string )
 
 init : ( Model, Cmd Msg )
-init = ( { query = "", search_result = Loading } , getSearchResult "" )
+init = ( { query = "", searchResult = Loading, sortMode = Relevance } , getSearchResult "" )
