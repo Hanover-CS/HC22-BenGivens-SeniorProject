a5f6130cd9831c9287de89c73249c225d5e61c84
Benjamin Givens <btg99email@gmail.com>
2022-02-11 10:02:36 -0500

    Fix build script compatibility issue

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 import subprocess
 
 print("elm make src/Main.elm --output app.js")
-subprocess.run(["elm", "make", "src/Main.elm", "--output", "app.js"], cwd = "app")
+subprocess.run(["elm", "make", "src/Main.elm", "--output", "app.js"], shell = True, cwd = "app")
 print("cargo build")
 subprocess.run(["cargo", "build", "--release"])
