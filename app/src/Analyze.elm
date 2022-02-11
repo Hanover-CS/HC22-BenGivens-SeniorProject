746a4603cf3dc2b292a4d857c18fc2f392d061a8
Benjamin Givens <btg99email@gmail.com>
2022-02-11 03:49:30 -0500

    Display length distribution chart

--- app/src/Analyze.elm
+++ app/src/Analyze.elm
@@ -8,22 +8,37 @@ import Http
 import Json.Decode as Decode exposing (Decoder)
 
 type Msg
-    = GotFrequency (Result Http.Error (List Frequency))
+    = GotFrequencies (Result Http.Error (List Frequency))
+    | GotLengths (Result Http.Error (List Float))
+
 type alias Frequency = { code : String, count : Float }
 
 type Model
-    = Loading
-    | Analyzing (List Frequency)    
+    = Loading PartialData
+    | Analyzing Data
+
+type alias PartialData =
+    { frequencies : Maybe ( List Frequency )
+    , lengths : Maybe ( List Float )
+    }
+
+type alias Data =
+    { frequencies : List Frequency
+    , lengths : List Float
+    }    
 
 init : ( Model, Cmd Msg )
 init =
-    ( Loading, getFrequency )
+    ( Loading initPartial, Cmd.batch [ getFrequencies, getLengths] )
 
-getFrequency : Cmd Msg
-getFrequency =
+initPartial : PartialData
+initPartial = PartialData Nothing Nothing
+
+getFrequencies : Cmd Msg
+getFrequencies =
     Http.get
         { url = "api/frequency"
-        , expect = Http.expectJson GotFrequency frequencyDecoder
+        , expect = Http.expectJson GotFrequencies frequencyDecoder
         }
 
 frequencyDecoder : Decoder (List Frequency)
@@ -33,25 +48,44 @@ frequencyDecoder =
             ( Decode.field "code" Decode.string )
             ( Decode.field "count" Decode.float )
 
+getLengths : Cmd Msg
+getLengths =
+    Http.get
+        { url = "api/length"
+        , expect = Http.expectJson GotLengths lengthDecoder
+        }
+
+lengthDecoder : Decoder (List Float)
+lengthDecoder =
+    Decode.list Decode.float
+
 view : Model -> Html Msg
 view model =
     case model of
-        Loading -> viewLoading
-        Analyzing data -> viewFrequency data
+        Loading _ -> viewLoading
+        Analyzing data -> viewData data
 
 viewLoading : Html Msg
 viewLoading =
     H.text "Loading"
 
-viewFrequency : List Frequency -> Html Msg
-viewFrequency data =
+viewData : Data -> Html Msg
+viewData data =
+    H.div []
+        [ viewFrequencies data.frequencies
+        , viewLengths data.lengths
+        ]
+
+viewFrequencies : List Frequency -> Html Msg
+viewFrequencies frequencies =
     H.div
-        []
+        [ HA.style "margin" "50px" ]
         [ H.text "Error Code Frequency"
         , H.div
             [ HA.style "height" "300px"
             , HA.style "width" "300px"
-            , HA.style "position" "absolute"
+            , HA.style "position" "relative"
+            , HA.style "display" "block"
             , HA.style "left" "50px"
             ]
             [ C.chart
@@ -63,14 +97,68 @@ viewFrequency data =
                 , C.bars
                     []
                     [ C.bar .count [] ]
-                    data
+                    frequencies
+                ]
+            ]
+        ]
+
+viewLengths : List Float -> Html Msg
+viewLengths lengths =
+    H.div
+        [ HA.style "margin" "50px" ]
+        [ H.text "Error Code Length"
+        , H.div
+            [ HA.style "height" "300px"
+            , HA.style "width" "300px"
+            , HA.style "position" "relative"
+            , HA.style "display" "block"
+            , HA.style "left" "50px"
+            ]
+            [ C.chart
+                [ CA.height 300
+                , CA.width 300
+                ]
+                [ C.yLabels [ CA.withGrid ]
+                , C.bars
+                    [ CA.margin 0 ]
+                    [ C.bar identity [] ]
+                    (lengths |> List.sort)
                 ]
             ]
         ]
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        GotFrequency (Ok frequency) -> ( Analyzing frequency, Cmd.none)
-        GotFrequency (Err _) -> ( Loading, Cmd.none )
+    thenNothing <|
+        case msg of
+            GotFrequencies (Ok frequencies) ->
+                let partial = downgradeData model
+                    newPartial = { partial | frequencies = Just frequencies }
+                in
+                    case upgradePartialData newPartial of
+                        Just data -> Analyzing data
+                        Nothing -> Loading newPartial
+            GotFrequencies (Err _) -> Loading initPartial
+            GotLengths (Ok lengths) ->
+                let partial = downgradeData model
+                    newPartial = { partial | lengths = Just lengths }
+                in
+                    case upgradePartialData newPartial of
+                        Just data -> Analyzing data
+                        Nothing -> Loading newPartial
+            GotLengths (Err _) -> Loading initPartial
     
+thenNothing : Model -> ( Model, Cmd Msg )
+thenNothing model = ( model, Cmd.none )
+
+downgradeData : Model -> PartialData
+downgradeData model =
+    case model of
+        Loading partial -> partial
+        Analyzing data -> PartialData (Just data.frequencies) (Just data.lengths)
+
+upgradePartialData : PartialData -> Maybe Data
+upgradePartialData partialData =
+    case ( partialData.frequencies, partialData.lengths ) of
+        ( Just frequencies, Just lengths ) -> Just (Data frequencies lengths)
+        _ -> Nothing
