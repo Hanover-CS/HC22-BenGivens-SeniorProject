d24b4a727635303cb4d1d817017618f35e974ab0
Benjamin Givens <btg99email@gmail.com>
2022-02-11 01:42:09 -0500

    Build in release mode to compute force graph relations

--- build.py
+++ build.py
@@ -3,4 +3,4 @@ import subprocess
 print("elm make src/Main.elm --output app.js")
 subprocess.run(["elm", "make", "src/Main.elm", "--output", "app.js"], cwd = "app")
 print("cargo build")
-subprocess.run(["cargo", "build"])
+subprocess.run(["cargo", "build", "--release"])
